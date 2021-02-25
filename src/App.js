b8287a6c1b0cf6e4a2d4708645d1cf81f98f7851
Alejandro <alejandro.becerra.pro@gmail.com>
2021-02-25 17:20:44 -0500

    mostrando la frase de la API

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
+import {useState} from 'react';
 import styled from '@emotion/styled';
+import Frase from './components/Frase';
 
 const Contenedor = styled.div`
   display: flex;
@@ -25,16 +27,23 @@ const Boton = styled.button`
 
 function App() {
 
+  //state de frases 
+
+  const [frase, guardarFrase] = useState({});
+
   const consultarAPI = async () => {
     const api = await fetch('https://breaking-bad-quotes.herokuapp.com/v1/quotes');
     
     const frase = await api.json();
   
-    console.log(frase)
+    guardarFrase(frase[0]);
   }
 
   return (
     <Contenedor>
+      <Frase
+        frase= {frase}
+      />
       <Boton
         onClick={consultarAPI}>
           Obtener Frase
